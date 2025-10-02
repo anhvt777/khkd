ab4b8c5d5f8beed5da6777dbaad2ab901f7dc72d
anhvt777 <167689872+anhvt777@users.noreply.github.com>
2025-10-02 17:09:20 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Đổi tên file này thành config.js và điền thông tin Supabase
+
 window.ENV = {
   SUPABASE_URL: "https://rrfkmvrkvvzobyokuvzp.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZmttdnJrdnZ6b2J5b2t1dnpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzc3MTQsImV4cCI6MjA3NDk1MzcxNH0.l_rsvKnNV5HfkdScIThB89s9ocqYdL8LtHGVHla4-jU"
